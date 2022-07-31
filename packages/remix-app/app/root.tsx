a727c1f0d45e3dc4e5d49d81d512409618de8baa
rokobekavac0 <roko.bekavac2003@gmail.com>
2022-07-31 23:15:56 +0200

    rm boilerplate

--- packages/remix-app/app/root.tsx
+++ packages/remix-app/app/root.tsx
@@ -1,9 +1,4 @@
-import type {
-  LinksFunction,
-  LoaderFunction,
-  MetaFunction,
-} from '@remix-run/cloudflare'
-import { json } from '@remix-run/cloudflare'
+import type { LinksFunction, MetaFunction } from '@remix-run/cloudflare';
 import {
   Links,
   LiveReload,
@@ -11,26 +6,19 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
-} from '@remix-run/react'
-import styles from '~/styles/app.css'
+} from '@remix-run/react';
+import styles from '~/styles/app.css';
 
 export let meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1',
   'color-scheme': 'dark light',
-})
+});
 
-export let links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
-
-export let loader: LoaderFunction = async ({}) => {
-  return json<any>({ sd: 2 })
-}
+export let links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
 export default function App() {
-  let loaderData = useLoaderData<LoaderData>()
-
   return (
     <html lang='en'>
       <head>
@@ -39,15 +27,10 @@ export default function App() {
       </head>
       <body>
         <Outlet />
-        <p className='text-xl'>Invocatoins: {loaderData.count}</p>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  )
-}
-
-type LoaderData = {
-  count: number
+  );
 }
