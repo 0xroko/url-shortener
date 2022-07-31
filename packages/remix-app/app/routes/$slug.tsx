4027a6ae59d3cf229e5f9aff1ee33ed33da00022
rokobekavac0 <roko.bekavac2003@gmail.com>
2022-07-31 23:15:09 +0200

    redirect page

---
+++ packages/remix-app/app/routes/$slug.tsx
@@ -0,0 +1,10 @@
+import type { LoaderFunction } from '@remix-run/cloudflare'
+import { redirect } from '@remix-run/cloudflare'
+
+export const loader: LoaderFunction = async ({ context, params }) => {
+  const slug = params.slug ?? ''
+
+  const url = await context.env.URLS.get(slug)
+
+  return redirect(url ?? '/')
+}
