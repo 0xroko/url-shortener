9709144169a5bd21ab2dd1ce90bac76f098e9db6
rokobekavac0 <roko.bekavac2003@gmail.com>
2022-08-01 21:09:40 +0200

    refactor

--- packages/remix-app/app/root.tsx
+++ packages/remix-app/app/root.tsx
@@ -11,7 +11,7 @@ import styles from '~/styles/app.css';
 
 export let meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: '',
   viewport: 'width=device-width,initial-scale=1',
   'color-scheme': 'dark light',
 });
