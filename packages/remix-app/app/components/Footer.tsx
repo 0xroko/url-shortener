da1a9058417430009756c9bcbab270ae6c3c01fc
rokobekavac0 <roko.bekavac2003@gmail.com>
2022-08-02 18:39:29 +0200

    fix footer padding

--- packages/remix-app/app/components/Footer.tsx
+++ packages/remix-app/app/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
       href='https://twitter.com/bekavac_roko'
       rel='noreferrer'
       target='_blank'
-      className={`my-5 w-full text-center leading-6 text-white text-opacity-50`}
+      className={`mt-10 mb-5 w-full text-center leading-6 text-white text-opacity-50 md:my-5`}
     >
       Made by <b>@rokobekavac</b>
     </a>
