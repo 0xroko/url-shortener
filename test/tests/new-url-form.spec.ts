cff925f6bd503700c4add796bded3892cfea862a
rokobekavac0 <roko.bekavac2003@gmail.com>
2022-08-06 22:03:10 +0200

    rm test

--- test/tests/new-url-form.spec.ts
+++ test/tests/new-url-form.spec.ts
@@ -7,25 +7,7 @@ test.describe('Create new url', () => {
 
   test('Invalid URL', async ({ page, baseURL }) => {
     // Click [placeholder="example\.com"]
-    await page.locator('[placeholder="example\\.com"]').click();
 
-    // Fill [placeholder="example\.com"]
-    await page.locator('[placeholder="example\\.com"]').fill('not url');
-
-    // Press Tab
-    await page.locator('[placeholder="example\\.com"]').press('Tab');
-
-    // Fill [data-test-id="slug-input"]
-    await page.locator('[data-test-id="slug-input"]').fill('slug');
-
-    // Press Tab
-    await page.locator('[data-test-id="slug-input"]').press('Tab');
-
-    // Fill [placeholder="Password"]
-    await page.locator('[placeholder="Password"]').fill('testing');
-
-    // Press Enter
-    await page.locator('[placeholder="Password"]').press('Enter');
     await expect(page).toHaveURL('http://localhost:8787/?index');
   });
 });
