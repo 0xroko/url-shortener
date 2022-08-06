e02bef0dd4b48d9b571a2363b63549ec706b78f6
rokobekavac0 <roko.bekavac2003@gmail.com>
2022-08-06 20:37:56 +0200

    fix text size

--- packages/remix-app/app/components/Hero.tsx
+++ packages/remix-app/app/components/Hero.tsx
@@ -3,15 +3,15 @@ import { appText } from '~/lib/const';
 export const Hero = () => {
   return (
     <div
-      className={`my-48 flex max-w-xl flex-grow-0 flex-col items-center justify-center gap-3 lg:my-0 lg:items-start`}
+      className={`my-48 flex max-w-xl flex-grow-0 flex-col items-center justify-center gap-4 lg:my-0 lg:items-start lg:gap-3`}
     >
       <div
-        className={`lg:text-start -mx-3 text-center text-[46px] font-semibold leading-[52px] tracking-[-0.015em] text-white text-opacity-[0.85] md:mx-0 lg:text-[64px] lg:leading-[76px] `}
+        className={`lg:text-start -mx-3 text-center text-[46px] font-semibold leading-[52px] tracking-[-0.015em] text-white text-opacity-[0.85] md:mx-0 md:text-[54px] md:leading-[60px]  xl:text-[64px] xl:leading-[76px] `}
       >
         {appText.hero.title}
       </div>
       <div
-        className={`lg:text-start max-w-xs text-center text-lg text-white text-opacity-50 sm:max-w-sm md:max-w-lg lg:max-w-none`}
+        className={`lg:text-start max-w-xs text-center text-lg text-white text-opacity-50 sm:max-w-sm md:max-w-lg xl:max-w-none`}
       >
         {appText.hero.subtitle}
       </div>
