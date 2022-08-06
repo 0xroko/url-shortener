9b30ba4fbf64bd29aaa4980405bed708593b6bd6
rokobekavac0 <roko.bekavac2003@gmail.com>
2022-08-06 20:36:07 +0200

    add toast

--- packages/remix-app/app/components/Form.tsx
+++ packages/remix-app/app/components/Form.tsx
@@ -3,68 +3,101 @@ import {
   useActionData,
   useTransition,
 } from '@remix-run/react';
+import { useEffect, useRef, useState } from 'react';
 
 import { extractFormError } from '~/lib/helpers';
 import type { ActionReturnType } from '~/routes';
 import { Button } from './Button';
 import { Input } from './Input';
+import { Toast } from './Toast';
 
 export const Form = () => {
   const transition = useTransition();
   const actionData = useActionData<ActionReturnType>();
-  return (
-    <div
-      className={`-mt-7 flex flex-col items-center justify-center lg:max-w-sm`}
-    >
-      <RemixFrom method='post'>
-        <fieldset disabled={transition.state === 'submitting'}>
-          <div className={`flex flex-col gap-6`}>
-            <div
-              className={`text-2xl font-semibold leading-8 text-white text-opacity-80`}
-            >
-              Create new link
-            </div>
-            <Input
-              description='Url you want to shorten'
-              name='url'
-              placeholder='example.com'
-              label='URL'
-              error={extractFormError('url', actionData?.inputErrorsFormatted)}
-            ></Input>
-            <div className={`flex gap-5`}>
-              <div className={`flex flex-[50%]`}>
-                <Input
-                  description='If not provided, random string will be used'
-                  name='slug'
-                  label='Slug*'
-                  placeholder='ytv'
-                  error={extractFormError(
-                    'slug',
-                    actionData?.inputErrorsFormatted,
-                  )}
-                ></Input>
-              </div>
-              <div className={`flex flex-[50%]`}>
-                <Input
-                  description='Private shortners require a key'
-                  name='key'
-                  type='password'
-                  placeholder='Password'
-                  label='Key'
-                  error={extractFormError(
-                    'key',
-                    actionData?.inputErrorsFormatted,
-                  )}
-                ></Input>
-              </div>
-            </div>
 
-            <Button disabled={transition.state === 'submitting'} type='submit'>
-              {transition.state === 'submitting' ? 'Creating...' : 'Create'}
-            </Button>
-          </div>
-        </fieldset>
-      </RemixFrom>
-    </div>
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const [toastOpen, setToastOpen] = useState(false);
+
+  const isAdding =
+    transition.state === 'submitting' &&
+    transition.submission.formData.get('_action') === 'create';
+
+  useEffect(() => {
+    if (!isAdding && !!actionData?.data) {
+      formRef.current?.reset();
+      setToastOpen(true);
+    }
+  }, [isAdding, actionData]);
+
+  return (
+    <>
+      <div
+        className={`-mt-7 flex max-w-md flex-col items-center justify-center lg:max-w-sm`}
+      >
+        <RemixFrom ref={formRef} method='post'>
+          <fieldset disabled={transition.state === 'submitting'}>
+            <div className={`flex flex-col gap-6`}>
+              <div
+                className={`text-2xl font-semibold leading-8 text-white text-opacity-80`}
+              >
+                Create new link
+              </div>
+              <Input
+                description='Url you want to shorten'
+                name='url'
+                placeholder='example.com'
+                label='URL'
+                error={extractFormError(
+                  'url',
+                  actionData?.inputErrorsFormatted,
+                )}
+              ></Input>
+              <div className={`flex gap-5`}>
+                <div className={`flex w-1/2`}>
+                  <Input
+                    description='If not provided, random string will be used'
+                    name='slug'
+                    label='Slug*'
+                    placeholder='ytv'
+                    error={extractFormError(
+                      'slug',
+                      actionData?.inputErrorsFormatted,
+                    )}
+                  ></Input>
+                </div>
+                <div className={`flex w-1/2`}>
+                  <Input
+                    description='Private shortners require a key'
+                    name='key'
+                    type='password'
+                    placeholder='Password'
+                    label='Key'
+                    error={extractFormError(
+                      'key',
+                      actionData?.inputErrorsFormatted,
+                    )}
+                  ></Input>
+                </div>
+              </div>
+
+              <Button
+                name='_action'
+                value='create'
+                disabled={transition.state === 'submitting'}
+                type='submit'
+              >
+                {transition.state === 'submitting' ? 'Creating...' : 'Create'}
+              </Button>
+            </div>
+          </fieldset>
+        </RemixFrom>
+      </div>
+      <Toast open={toastOpen} onOpenChange={setToastOpen}>
+        <Toast.Title>
+          Link <b>{actionData?.data?.fullUrl} </b> added
+        </Toast.Title>
+      </Toast>
+    </>
   );
 };
--- packages/remix-app/tailwind.config.js
+++ packages/remix-app/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   darkMode: 'media',
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
@@ -11,5 +13,53 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function (helpers) {
+      // variants that help styling Radix-UI components
+      dataVariant('data-swipe', 'open', helpers);
+      dataVariant('data-swipe', 'closed', helpers);
+      dataVariant('data-swipe', 'move', helpers);
+      dataVariant('data-swipe', 'cancel', helpers);
+      dataVariant('data-swipe', 'end', helpers);
+    }),
+  ],
 };
+
+function dataVariant(
+  variantName,
+  state,
+  {
+    addVariant, // for registering custom variants
+    e, // for manually escaping strings meant to be used in class names
+  },
+) {
+  addVariant(`${variantName}-${state}`, ({ modifySelectors, separator }) => {
+    modifySelectors(({ className }) => {
+      return `.${e(
+        `${variantName}-${state}${separator}${className}`,
+      )}[${variantName}='${state}']`;
+    });
+  });
+
+  addVariant(
+    `group-${variantName}-${state}`,
+    ({ modifySelectors, separator }) => {
+      modifySelectors(({ className }) => {
+        return `.group[${variantName}='${state}'] .${e(
+          `group-${variantName}-${state}${separator}${className}`,
+        )}`;
+      });
+    },
+  );
+
+  addVariant(
+    `peer-${variantName}-${state}`,
+    ({ modifySelectors, separator }) => {
+      modifySelectors(({ className }) => {
+        return `.peer[${variantName}='${state}'] ~ .${e(
+          `peer-${variantName}-${state}${separator}${className}`,
+        )}`;
+      });
+    },
+  );
+}
