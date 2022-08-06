198852333984a6d9fe5467b72654acbc9902d416
rokobekavac0 <roko.bekavac2003@gmail.com>
2022-08-06 20:37:45 +0200

    add focus ring

--- packages/remix-app/app/components/Button.tsx
+++ packages/remix-app/app/components/Button.tsx
@@ -6,7 +6,7 @@ export const Button = ({ children, ...props }: ButtonProps) => {
   return (
     <button
       {...props}
-      className={`flex h-9 w-full items-center justify-center gap-2 rounded-2xl bg-white  text-center font-medium text-black duration-300 hover:bg-opacity-90 focus:outline-black disabled:bg-opacity-80  disabled:opacity-80`}
+      className={`flex h-9 w-full items-center justify-center gap-2 rounded-2xl bg-white text-center font-medium text-black transition-all duration-300 hover:bg-opacity-90 focus:ring-1 focus:ring-white focus:ring-offset-1 focus:ring-offset-black disabled:bg-opacity-80 disabled:opacity-80`}
     >
       {children}
     </button>
