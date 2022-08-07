8547f3cf0d2ee44f823bb3f8501c70352dbe0253
rokobekavac0 <roko.bekavac2003@gmail.com>
2022-08-07 20:52:03 +0200

    refactro

--- packages/remix-app/app/components/Hero.tsx
+++ packages/remix-app/app/components/Hero.tsx
@@ -6,12 +6,12 @@ export const Hero = () => {
       className={`my-48 flex max-w-xl flex-grow-0 flex-col items-center justify-center gap-4 lg:my-0 lg:items-start lg:gap-3`}
     >
       <div
-        className={`-mx-3 text-center text-[46px] font-semibold leading-[52px] tracking-[-0.015em] text-white text-opacity-[0.85] md:mx-0 md:text-[54px] md:leading-[60px] lg:text-start  xl:text-[64px] xl:leading-[76px] `}
+        className={`lg:text-start -mx-3 text-center text-[46px] font-semibold leading-[52px] tracking-[-0.015em] text-white text-opacity-[0.85] md:mx-0 md:text-[54px] md:leading-[60px]  xl:text-[64px] xl:leading-[76px] `}
       >
         {appText.hero.title}
       </div>
       <div
-        className={`max-w-xs text-center text-lg text-white text-opacity-50 sm:max-w-sm md:max-w-lg lg:text-start xl:max-w-none`}
+        className={`lg:text-start max-w-xs text-center text-lg text-white text-opacity-50 sm:max-w-sm md:max-w-lg xl:max-w-none`}
       >
         {appText.hero.subtitle}
       </div>
