f8afea7f4e8dd4d2f3ee5b74bef404eeac0ef8ec
Roko <55251261+0xroko@users.noreply.github.com>
2023-07-14 11:00:12 +0200

    remove twitter

--- packages/remix-app/app/components/Footer.tsx
+++ packages/remix-app/app/components/Footer.tsx
@@ -5,17 +5,8 @@ export const Footer = () => {
     <div
       className={`mt-10 mb-5 w-full  text-center leading-6 text-white text-opacity-50 md:my-5`}
     >
-      <a
-        href={appText.socials.twitterLink}
-        rel='noreferrer'
-        target='_blank'
-        className={``}
-      >
-        Made by <span>{appText.socials.twitterName}</span>
-      </a>
-      ・
       <a rel='noreferrer' href={appText.socials.githubLink} target='_blank'>
-        GitHub
+        GitHub repo
       </a>
     </div>
   );
